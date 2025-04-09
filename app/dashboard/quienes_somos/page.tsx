9ec27295ef6fdc50d015452446b9ed3745c65752
LuisGonzalesVentura <luis.florencio.gonzales.ventura@gmail.com>
2025-04-09 14:50:57 -0400

    correccion de error

---
+++ app/dashboard/quienes_somos/page.tsx
@@ -0,0 +1,8 @@
+// app/dashboard/quienes_somos/page.tsx
+import React from 'react';
+
+const QuienesSomos = () => {
+  return <div>Contenido de la página Quiénes Somos</div>;
+};
+
+export default QuienesSomos;
